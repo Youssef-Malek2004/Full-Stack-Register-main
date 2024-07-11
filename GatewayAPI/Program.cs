bce915e030ce214338ca4db5d32dd30775af674d
Youssef-Malek2004 <161763492+Youssef-Malek2004@users.noreply.github.com>
2024-07-11 17:07:37 +0300

    Program.cs-appsetting-configured

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using Application.DTOs.UserDTOs;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,6 +36,22 @@ app.MapPost("/register", async (HttpRequest request, HttpResponse response) =>
         // Return the error response content back to the frontend
         await response.WriteAsJsonAsync(errorResponse);
     }
+});
+
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+    busConfigurator.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
+        {
+            h.Username(builder.Configuration["MessageBroker:Username"]!);
+            h.Password(builder.Configuration["MessageBroker:Password"]!);
+        });
+
+        configurator.ConfigureEndpoints(context);
+    });
 });
 
 app.Run();
--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,7 @@ using Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Application.CommandHandlers;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,22 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Cr
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+    busConfigurator.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
+        {
+            h.Username(builder.Configuration["MessageBroker:Username"]!);
+            h.Password(builder.Configuration["MessageBroker:Password"]!);
+        });
+
+        configurator.ConfigureEndpoints(context);
+    });
 });
 
 var app = builder.Build();
